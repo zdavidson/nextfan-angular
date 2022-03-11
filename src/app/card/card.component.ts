52ea0c077a4db15ac2a8ae0d37b9167848613908
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2022-03-11 10:11:17 -0500

    cards populating

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  items = [
+    {
+      imageUrl: '',
+      name: 'item1',
+      price: 345,
+      description: '',
+      itemCount: 234,
+      createdDate: new Date().valueOf(),
+    },
+    {
+      imageUrl: '',
+      name: 'item2',
+      price: 345,
+      description: '',
+      itemCount: 234,
+      createdDate: new Date().valueOf(),
+    },
+    {
+      imageUrl: '',
+      name: 'item3',
+      price: 345,
+      description: '',
+      itemCount: 234,
+      createdDate: new Date().valueOf(),
+    },
+    {
+      imageUrl: '',
+      name: 'item4',
+      price: 345,
+      description: '',
+      itemCount: 234,
+      createdDate: new Date().valueOf(),
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
