8da005c108d7fc96c4accf76223ee268799518dd
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2022-03-11 08:33:44 -0500

    step4 complete

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http2ServerResponse } from 'http2';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+
+interface Card {}
 
 @Injectable({
   providedIn: 'root',
@@ -8,5 +9,83 @@ import { Observable } from 'rxjs';
 export class ApiService {
   constructor() {}
 
-  getCards(): Observable {}
+  public getCards(): Observable<Card[]> {
+    const items: Card[] = [
+      {
+        imageUrl: '',
+        name: '',
+        price: 345,
+        description: '',
+        itemCount: 234,
+        createdDate: new Date().valueOf(),
+      },
+      {
+        imageUrl: '',
+        name: '',
+        price: 345,
+        description: '',
+        itemCount: 234,
+        createdDate: new Date().valueOf(),
+      },
+      {
+        imageUrl: '',
+        name: '',
+        price: 345,
+        description: '',
+        itemCount: 234,
+        createdDate: new Date().valueOf(),
+      },
+      {
+        imageUrl: '',
+        name: '',
+        price: 345,
+        description: '',
+        itemCount: 234,
+        createdDate: new Date().valueOf(),
+      },
+    ];
+    return of(items);
+  }
+
+  promiseGetCards() {
+    const items = [
+      {
+        imageUrl: '',
+        name: '',
+        price: 345,
+        description: '',
+        itemCount: 234,
+        createdDate: new Date().valueOf(),
+      },
+      {
+        imageUrl: '',
+        name: '',
+        price: 345,
+        description: '',
+        itemCount: 234,
+        createdDate: new Date().valueOf(),
+      },
+      {
+        imageUrl: '',
+        name: '',
+        price: 345,
+        description: '',
+        itemCount: 234,
+        createdDate: new Date().valueOf(),
+      },
+      {
+        imageUrl: '',
+        name: '',
+        price: 345,
+        description: '',
+        itemCount: 234,
+        createdDate: new Date().valueOf(),
+      },
+    ];
+
+    let promise = new Promise((resolve, reject) => {
+      resolve(items);
+    });
+    return promise;
+  }
 }
